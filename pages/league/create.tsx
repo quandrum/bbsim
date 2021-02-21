440f8ee848df3a5412ce1acd799262e97d71ce7c
Adam Sinnett <quandrum@gmail.com>
2021-02-20 16:24:58 -0800

    feat: basic validation on league create

--- pages/league/create.tsx
+++ pages/league/create.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { Button } from '../../components/base';
 import { CREATE_LEAGUE } from '../../queries';
 import { useAuth } from '../../util/useAuth';
+import * as Yup from 'yup';
 
 const initialValues = {
   name: '',
@@ -14,6 +15,14 @@ interface League {
   name: string;
   year: number;
 }
+
+const schema = Yup.object().shape({
+  name: Yup.string().min(4).max(50).required(),
+  year: Yup.number()
+    .min(1946)
+    .max(initialValues.year + 1)
+    .required(),
+});
 
 const CreateLeague: React.FC = () => {
   const { user } = useAuth();
@@ -28,11 +37,17 @@ const CreateLeague: React.FC = () => {
   };
 
   return (
-    <Formik initialValues={initialValues} onSubmit={onSubmit}>
-      {({ handleSubmit }) => (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+      validationSchema={schema}
+    >
+      {({ errors, touched, handleSubmit }) => (
         <>
           <Field name="name" placeholder="League Name" />;
+          {errors.name && touched.name && <div>{errors.name}</div>}
           <Field type="number" name="year" step="1" placeholder="Start Year" />
+          {errors.year && touched.year && <div>{errors.year}</div>}
           <Button type="submit" onClick={() => handleSubmit()}>
             Create
           </Button>
