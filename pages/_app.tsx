e8d828a7d8544043cce2278cb8c8ba6135d02114
Adam Sinnett <quandrum@gmail.com>
2021-02-21 14:53:18 -0800

    bugfix: add log rocket id

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,7 +8,7 @@ import setupLogRocketReact from 'logrocket-react';
 
 // only initialize when in the browser
 if (typeof window !== 'undefined') {
-  LogRocket.init('app/id');
+  LogRocket.init('ffljx6/bbsim');
   // plugins should also only be initialized when in the browser
   setupLogRocketReact(LogRocket);
 }
